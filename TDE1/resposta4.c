aadd9a83db62fb0ee7e25e347bc61e6a4bc1f586
roberto-braga <roberto.braga0403@gmail.com>
2024-11-07 20:33:11 -0300

    resposta4.c

---
+++ TDE1/resposta4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAX_PRODUTOS 5
+#define NOME_MAX 20
+
+typedef struct {
+    int id;
+    char nome[NOME_MAX];
+    float valor;
+} Produto;
+
+int main() {
+    Produto produtos[MAX_PRODUTOS];
+    int i;
+    float soma = 0.0;  
+
+    for (i = 0; i < MAX_PRODUTOS; i++) {
+        printf("Digite o ID do produto %d: ", i + 1);
+        scanf("%d", &produtos[i].id);
+        printf("Digite o nome do produto %d: ", i + 1);
+        scanf("%s", produtos[i].nome);
+        printf("Digite o valor do produto %d: ", i + 1);
+        scanf("%f", &produtos[i].valor);
+        
+        soma += produtos[i].valor;
+    }
+
+    float media = soma / MAX_PRODUTOS;
+
+    printf("A media dos precos dos produtos eh: %.2f\n", media);
+
+    return 0;
+}
